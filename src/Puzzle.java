a3f088b40a15115289faf9b98ea34308993083d8
mohit1101988 <mohit1101988@gmail.com>
2017-05-29 19:48:57 +0530

    Initial Commit

---
+++ src/Puzzle.java
@@ -0,0 +1,61 @@
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class Puzzle {
+
+	public static void main(String[] args) {
+		
+		
+		/*
+		 * Take the input array
+		 * Get the size of the array
+		 * For each element, pair it with all other elements of the array and store their sum in a key value pair
+		 * Store above in a hash map
+		 * compare the values of each  
+		 * give out the result
+		 */
+
+		int []input = {1,4,2,3,8,9,5};
+		int length = input.length;
+		
+		//To put all addition pairs and their values in a hashTable
+		Hashtable<String, Integer> myData  = new Hashtable<String, Integer>() ; 
+		
+		for (int i = 0; i < length; i++) {
+			for (int j = i+1; j < length; j++) {
+				myData.put(input[i]+"+"+input[j], input[i]+input[j]);
+			}
+		}
+		
+		//To be used for printing out the Hashtable entries, if needed
+		
+		/*for (Map.Entry<String, Integer> entry : myData.entrySet()) {
+		    String key = entry.getKey();
+		    Integer value = entry.getValue();
+
+		    System.out.println ("Key: " + key + " Value: " + value);
+		}*/
+		
+		//For comparing the values and printout the result
+		
+		for(Map.Entry<String, Integer> entry1: myData.entrySet()) {
+			   String key1 = entry1.getKey();
+			   int value1 = entry1.getValue();
+			   for(Map.Entry<String, Integer> entry2: myData.entrySet()) {
+				   String key2 = entry2.getKey();
+				   int value2 = entry2.getValue();
+				   if (key1.hashCode() > key2.hashCode()){
+			    	   if(value1 == value2){
+			    		   System.out.println(entry1.getKey()+" is equal to "+entry2.getKey());
+			    	   }
+			       }
+
+			   }
+			}
+		
+	
+		
+	}
+
+}
